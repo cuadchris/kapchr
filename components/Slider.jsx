e7a8d80b15eeb7cc2278b363787814ccd7435b81
Christopher <cuadradochris@hotmail.com>
2022-12-16 16:24:39 -0500

    instagram layout

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -10,9 +10,8 @@ const Slider = ({ slides }) => {
   const nextSlide = () => {
     setCurrent(current === length - 1 ? 0 : current + 1);
   };
-
   const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current + 1);
+    setCurrent(current === 0 ? length - 1 : current - 1);
   };
 
   if (!Array.isArray(slides) || slides.length <= 0) {
@@ -30,13 +29,13 @@ const Slider = ({ slides }) => {
               className={
                 index === current
                   ? "opacity-[1] ease-in duration-1000"
-                  : "opacity-0 "
+                  : "opacity-0"
               }
             >
               <FaArrowCircleLeft
                 onClick={prevSlide}
-                size={50}
                 className="absolute top-[50%] left-[30px] text-white/70 cursor-pointer select-none z-[2]"
+                size={50}
               />
               {index === current && (
                 <Image
@@ -44,13 +43,13 @@ const Slider = ({ slides }) => {
                   alt="/"
                   width="1440"
                   height="600"
-                  style={{ objectfit: "cover" }}
+                  objectFit="cover"
                 />
               )}
               <FaArrowCircleRight
                 onClick={nextSlide}
-                size={50}
                 className="absolute top-[50%] right-[30px] text-white/70 cursor-pointer select-none z-[2]"
+                size={50}
               />
             </div>
           );
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Hero from "../components/Hero";
+import Instagram from "../components/instagram";
 import Slider from "../components/Slider";
 import { SliderData } from "../components/SliderData";
 
@@ -17,6 +18,7 @@ export default function Home() {
         message="I capture moments, and I keep them alive."
       />
       <Slider slides={SliderData} />
+      <Instagram/>
     </>
   );
 }
