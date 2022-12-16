42fd95c612aaf50acad073de3e13f830b5b61c62
Christopher <cuadradochris@hotmail.com>
2022-12-16 17:16:59 -0500

    edit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Hero from "../components/Hero";
-import Instagram from "../components/instagram";
+import Instagram from "../components/Instagram";
 import Slider from "../components/Slider";
 import { SliderData } from "../components/SliderData";
 
